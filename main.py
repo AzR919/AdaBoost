69281fd5193ba9a98854e8c7f547f454bd948d45
Azaan Rehman <azaanrehman919@gmail.com>
2020-11-03 21:38:01 -0500

    somewhat working. correct validation, wrong accuracies

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ class Data():
         self.train_root = data_train_root
         self.test_root = data_test_root
 
+        self.load_data()
+
     def load_data(self):
 
         train_raw = np.loadtxt(open(self.train_root, "rb"), delimiter=",")
@@ -25,11 +27,120 @@ class Data():
         self.test_l = test_raw[:,-1]
 
 class Ada_boost():
-    def __init__(self, num_samples, num_epochs):
+    def __init__(self, train_f_shape, num_epochs):
 
-        self.weights = np.ones((num_samples,1))
+        self.num_epochs = num_epochs
+
+        self.D = np.ones((train_f_shape[0])) 
+        self.D_sum = np.sum(self.D)
 
         self.beta = np.zeros((num_epochs))
+        self.weaks = np.zeros((num_epochs, 3))
+
+    def pred_trans(self, prem_pred):
+
+        return prem_pred*2-1
+
+    def best_weak(self, data):
+
+        train_f = np.copy(data.train_f)
+
+        best_e = np.inf
+
+        best_dir = 0
+        best_dim = 0
+        best_val = 0
+        best_ind = 0
+
+
+        for i in range(train_f.shape[0]):
+
+            point = train_f[i]
+
+            for j in range(train_f.shape[1]):
+
+                dim_data = train_f[:,j]
+
+                split_val = point[j]
+
+                e1 =  (np.sum ( self.D * (data.train_l == self.pred_trans(dim_data > split_val)) ) ) / self.D_sum
+
+                e2 =  (np.sum ( self.D * (data.train_l == self.pred_trans(dim_data <= split_val)) ) ) / self.D_sum
+
+                better_e, better_dir = (e1, 1) if e1 < e2 else (e2, 0)
+
+                if better_e < best_e :
+
+                    best_e = better_e
+                    best_dir = better_dir
+                    best_dim = j
+                    best_val = split_val
+                    best_ind = i
+
+
+        return best_val, best_dim, best_dir, best_e
+
+
+    def update_D (self, data, weak_l, b_t):
+
+        val, dim, dir = weak_l
+       
+        if dir == 1:
+            preds = self.pred_trans(data.train_f[:,dim] > val)
+        else:
+            preds = self.pred_trans(data.train_f[:,dim] <= val)
+
+        self.D *= np.exp(data.train_l * b_t * preds)
+        self.D_sum = np.sum(self.D)
+
+    
+    def train(self, data):
+
+        for i in range(self.num_epochs):
+            print("Begin Train Epoch : {}/{}".format(i+1, self.num_epochs))
+
+            val, dim, dir, e = self.best_weak(data)
+
+            self.beta[i] = (np.log( (1-e))-np.log(e))/2
+
+            self.weaks[i,0] = val
+            self.weaks[i,1] = dim
+            self.weaks[i,2] = dir
+
+            self.update_D(data, (val, dim, dir), self.beta[i])
+
+            print("Data in Epoch:= val:{}, dim:{}, dir:{}, e:{}".format(val, dim, dir, e))
+
+            print("Train acc : {}, Test acc : {}".format(self.eval_model(data.train_f, data.train_l), self.eval_model(data.test_f, data.test_l)))
+
+            print("-----------------------------------------")
+
+    def ada_predict(self, sub_data):
+
+        preds = np.zeros((sub_data.shape[0]))
+
+        for i in range(self.num_epochs):
+
+            val = self.weaks[i,0]
+            dim = int(self.weaks[i,1])
+            dir = self.weaks[i,2]
+
+            if dir == 1:
+                preds_i = self.pred_trans(sub_data[:,dim] > val)
+            else:
+                preds_i = self.pred_trans(sub_data[:,dim] <= val)
+
+            preds += self.beta[i] * preds_i
+
+        return self.pred_trans(preds>=0)
+
+    def eval_model(self, sub_data, sub_l):
+
+        preds = self.ada_predict(sub_data)
+
+        return sum(preds == sub_l)/sub_data.shape[0]
+
+
 
 
 def arg_parser():
@@ -38,11 +149,7 @@ def arg_parser():
     parser.add_argument("--train_root", type=str, default='../datasets/train_adaboost.csv', help="path to the training data")
     parser.add_argument("--test_root", type=str, default='../datasets/test_adaboost.csv', help="path to the test data")
 
-    parser.add_argument("--num_epochs", type=int, default=15, help='number of epochs to train for')
-    parser.add_argument("--step_size", type=float, default=0.01, help='step size for SGD')
-    parser.add_argument("--hidden_dim", type=int, default=256, help='dimension for the hidden layer')
-    parser.add_argument("--seed", type=int, default=0, help='seed for randomization')
-    parser.add_argument("--eps", type=float, default=1e-8, help='eps to prevent negative log')
+    parser.add_argument("--num_epochs", type=int, default=10, help='number of epochs to train for')
 
     args = parser.parse_args()
 
@@ -63,6 +170,18 @@ if __name__ == "__main__":
 
     data = Data(args.train_root, args.test_root)
 
+    print("-----------------------------------------")
+    print("Finish Loading Data")
+    print("-----------------------------------------")
+
+    model = Ada_boost(data.train_f.shape, args.num_epochs)
+
+    print("-----------------------------------------")
+    print("Begin Training")
+    print("-----------------------------------------")
+
+    model.train(data)
+    #import ipdb; ipdb.set_trace()
     print("-----------------------------------------")
     print("Finish Loading Data")
     print("-----------------------------------------")
